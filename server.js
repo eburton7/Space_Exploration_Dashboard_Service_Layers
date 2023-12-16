2e9ba522f6978b6ba92a218a6f969e9cffcd647a
Elysia Burton <elysialeann@gmail.com>
2023-12-16 07:27:22 -0500

    changed server port

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/fetch-solar-flares', async (req, res) => {
 });
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
